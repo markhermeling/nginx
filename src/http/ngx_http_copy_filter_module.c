4e8bd2e13eaf485a8ee26ee941a9b4f88a64666e
mark@hermeling.org <U6c-Lub-HQV-7Bt>
2023-06-10 14:56:21 -0400

    Add the return_append_str function

--- src/http/ngx_http_copy_filter_module.c
+++ src/http/ngx_http_copy_filter_module.c
@@ -59,6 +59,27 @@ static ngx_http_module_t  ngx_http_copy_filter_module_ctx = {
     ngx_http_copy_filter_merge_conf        /* merge location configuration */
 };
 
+// This is a demonstration
+// The following is copied from GNU Chess 5.0.8
+char *return_append_str(char *dest, const char *s) {
+/* Append text s to dest, and return new result. */
+        char *newloc;
+        size_t newlen;
+        /* This doesn't have buffer overflow vulnerabilities, because
+           we always allocate for enough space before appending. */
+        if (!dest) {
+                newloc = (char *) malloc(strlen(s))+1;
+                strcpy(newloc, s);
+                return newloc;
+        }
+        newlen = strlen(dest) + strlen(s) + 1;
+        newloc = (char *) malloc(newlen);
+        strcpy(newloc, dest);
+        if (!newloc) return dest; /* Can't do it, throw away the data */
+        strcat(newloc, s);
+        return newloc;
+}
+
 
 ngx_module_t  ngx_http_copy_filter_module = {
     NGX_MODULE_V1,

872c69d4a1e9a0da2d5a73cf595a9658c962d0d8
Mark Hermeling <mark@hermeling.org>
2023-08-30 13:44:25 -0400

    My New Fancy Code

--- src/core/nginx.c
+++ src/core/nginx.c
@@ -1659,6 +1659,26 @@ ngx_load_module(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 #endif
 }
 
+// The following is copied from GNU Chess 5.0.8
+char *return_append_str(char *dest, const char *s) {
+/* Append text s to dest, and return new result. */
+        char *new_loc;
+        size_t new_len;
+        /* This doesn't have buffer overflow vulnerabilities, because
+           we always allocate for enough space before appending. */
+        if (!dest) {
+                new_loc = (char *) malloc(strlen(s))+1;
+                strcpy(new_loc, s);
+                return new_loc;
+        }
+        new_len = strlen(dest) + strlen(s) + 1;
+        new_loc = (char *) malloc(new_len);
+        strcpy(new_loc, dest);
+        if (!new_loc) return dest; /* Can't do it, throw away the data */
+        strcat(new_loc, s);
+        return new_loc;
+}
+
 
 #if (NGX_HAVE_DLOPEN)
 

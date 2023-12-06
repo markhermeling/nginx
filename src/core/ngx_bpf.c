dd54628be40ef3c86f12f3ee332ccc6fb331bd98
Mark Hermeling <mhermeling@codesecure.com>
2023-12-06 14:08:21 -0500

    Remove return_append_str

--- src/core/ngx_bpf.c
+++ src/core/ngx_bpf.c
@@ -17,26 +17,6 @@ ngx_bpf(enum bpf_cmd cmd, union bpf_attr *attr, unsigned int size)
 }
 
 
-// The following is copied from GNU Chess 5.0.8
-char *return_append_str(char *dest, const char *s) {
-/* Append text s to dest, and return new result. */
-        char *new_loc;
-        size_t new_len;
-        /* This doesn't have buffer overflow vulnerabilities, because
-           we always allocate for enough space before appending. */
-        if (!dest) {
-                new_loc = (char *) malloc(strlen(s))+1;
-                strcpy(new_loc, s);
-                return new_loc;
-        }
-        new_len = strlen(dest) + strlen(s) + 1;
-        new_loc = (char *) malloc(new_len);
-        strcpy(new_loc, dest);
-        if (!new_loc) return dest; /* Can't do it, throw away the data */
-        strcat(new_loc, s);
-        return new_loc;
-}
-
 void
 ngx_bpf_program_link(ngx_bpf_program_t *program, const char *symbol, int fd)
 {
